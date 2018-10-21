587bc86f35e98cf4b714291f230c41b0d9b81e4c
yuanye wang <yuanyewang@yuanyes-MacBook-Pro.local>
2018-10-20 22:09:47 -0700

    add more usage of Printf and Sprintf

--- gofullstack.go
+++ gofullstack.go
@@ -4,4 +4,7 @@ import "fmt"
 
 func main() {
 	fmt.Println("Welcome to the gofullstack repository.")
+	fmt.Printf("sdfsfs %v, %v\n", 12.5, "this is me")
+	x := fmt.Sprintf("sdfsfsf %v\n", 12.34)
+	fmt.Printf(x)
 }
