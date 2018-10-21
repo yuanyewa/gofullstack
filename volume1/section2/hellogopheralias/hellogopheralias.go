803e7a4c6b459b81c1f8d5acdd2dccbb51b90159
Fake Person <afakeid@yahoo.com>
2018-10-21 10:21:42 -0700

    my import

--- volume1/section2/hellogopheralias/hellogopheralias.go
+++ volume1/section2/hellogopheralias/hellogopheralias.go
@@ -1,10 +1,15 @@
 // An example of aliasing the fmt package as f.
 package main
 
-import f "fmt"
+import (
+	f "fmt"
+
+	g "github.com/yuanyewa/gofullstack/volume1/section2/greetingspackage"
+)
 
 func main() {
 
 	f.Println("Hello Gopher!")
+	g.PrintGreetings()
 
 }
