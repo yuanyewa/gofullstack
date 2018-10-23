1318b7f80f3aa01b56d078c4d3bd4d4675beaef4
Fake Person <afakeid@yahoo.com>
2018-10-22 23:01:19 -0700

    my const type

--- volume1/section2/olympicslaonwards/olympicslaonwards.go
+++ volume1/section2/olympicslaonwards/olympicslaonwards.go
@@ -1,10 +1,44 @@
 // An example of using Go's iota enumerator to model the year and city where the summer Olympics took place.
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
+
+// iota increate by 1 each time; omit expression will adopt from previous one
+type myConst int
+
+const (
+	x myConst = 10 + iota
+	y         = 20 + iota
+	_
+	z
+)
+
+type myInt int
+
+func (m myInt) String() string {
+	return strconv.Itoa(int(m) * 10)
+}
+
+// type that implements a String() function can be printed accordingly
+func (m myConst) String() string {
+	switch m {
+	case x:
+		return "myX"
+	case y:
+		return "y"
+	case z:
+		return "z"
+	default:
+		return "nothing"
+	}
+}
 
 func main() {
-
+	fmt.Println(myInt(20))
+	fmt.Println(x)
 	const (
 		LosAngeles = 1984 + (iota * 4)
 		Seoul
