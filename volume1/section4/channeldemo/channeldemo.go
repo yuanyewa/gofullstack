b580e8540e51e659b5e1432653ac1d4d25228175
Fake Person <afakeid@yahoo.com>
2018-10-27 23:31:28 -0700

    my channel

--- volume1/section4/channeldemo/channeldemo.go
+++ volume1/section4/channeldemo/channeldemo.go
@@ -1,10 +1,11 @@
 // An example of sending and receiving a value through a channel.
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func writeMessageToChannel(message chan string) {
-
 	message <- "Hello Gopher!"
 
 }
@@ -14,9 +15,11 @@ func main() {
 	fmt.Println("Channel Demo")
 
 	message := make(chan string)
+	defer close(message)
 	go writeMessageToChannel(message)
 
+	val := make(chan int, 2)
+	go func() { val <- 10; val <- 20 }()
+	fmt.Println(<-val, <-val)
 	fmt.Println("Greeting from the message channel: ", <-message)
-
-	close(message)
 }
